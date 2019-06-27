191813b88e8ca41769f54d3cc50e7abde91c3f69
HugoRoca <hugo.rock20@hotmail.com>
2019-06-27 16:26:11 -0500

    se agrego controlador y route

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').Server(app);
-
-const io = require('socket.io');
+const http = require("http").Server(app);
+const io = require("socket.io");
 const port = 2705;
 const socket = io(http);
+const chatRouter = require("./server/routes/chat");
 
+app.use("/chat", chatRouter);
 app.use(express.static(`${__dirname}/client`));
 
 const chatSchema = require("./server/mongoDB/chatSchema");
 const connectDB = require("./server/mongoDB/dbconnection");
 
-socket.on('connection', (socket) => {
-    console.log('usuario conectado');
+socket.on("connection", (socket) => {
+    console.log("usuario conectado");
 
-    socket.on('disconnect', () => {
-        console.log('usuario desconectado');
+    socket.on("disconnect", () => {
+        console.log("usuario desconectado");
     });
 
-    socket.on('chat message', (msg) => {
+    socket.on("chat message", (msg) => {
         console.log(`message: ${msg}`);
 
-        socket.broadcast.emit('received', {
+        socket.broadcast.emit("received", {
             message: msg
         });
 
